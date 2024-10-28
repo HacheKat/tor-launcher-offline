3fb1dc687f0e68bba9e0c44ed97a6a2210dbaa5c
HacheKat <hache.cat@proton.me>
2024-10-28 07:57:08 +0100

    Delete execute_all.sh

--- execute_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exécution du premier script (téléchargement)
-chmod +x download.sh && bash download.sh
-
-# Exécution du second script (installation hors ligne)
-chmod +x install.sh && bash install.sh
